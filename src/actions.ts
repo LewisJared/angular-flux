1d3cab0972c71f743a246a6adb0ebedf817ff71c
Jared Lewis <jared@jared.kiwi.nz>
2015-06-10 08:37:16 +1200

    Added a dispatcher and constants

---
+++ src/actions.ts
@@ -0,0 +1,9 @@
+/// <reference path="dispatcher.ts" />
+// <reference path="constants.ts" />
+import Dispatcher = require('./dispatcher');
+import Constants = require('./constants');'
+
+export class Actions {
+
+}
+
